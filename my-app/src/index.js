852c1d61dac073fab5e44eac09c4362fd139444b
Michael Kirk <michael_c_kirk@yahoo.com>
2022-04-13 17:35:40 -0700

    removed unnecessary files

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
 
