750e126a3ad12193362060af86cc55f6f47e04dd
Michael Kirk <michael_c_kirk@yahoo.com>
2022-04-13 22:50:09 -0700

    Created simple nav bar

---
+++ my-app/src/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/projects">Projects</a>
+                <a href="/technicalskills">Technical Skills</a>
+                <a href="/contact">Contact</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
-
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
