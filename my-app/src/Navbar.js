142fb5460704e5cfa634b20c687ef23a0d34ee0d
Michael Kirk <michael_c_kirk@yahoo.com>
2022-04-15 00:10:24 -0700

    Added socials icons to nav bar

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -1,3 +1,6 @@
+import { IconContext } from "react-icons";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+
 function Navbar() {
     return (
         <nav className="navbar">
@@ -6,6 +9,17 @@ function Navbar() {
                 <a href="/projects">Projects</a>
                 <a href="/technicalskills">Technical Skills</a>
                 <a href="/contact">Contact</a>
+            </div>
+
+            <div className="socials">
+                <IconContext.Provider value={{ color: "white" }}>
+                    <a href="https://github.com/Michael-C-Kirk">
+                        <BsGithub />
+                    </a>
+                    <a href="https://www.linkedin.com/in/michael-kirk-4211a0152/">
+                        <BsLinkedin />
+                    </a>
+                </IconContext.Provider>
             </div>
         </nav>
     );
