0a6c25047d79976987fcb9c9ee4b80c11afa54de
Michael Kirk <michael_c_kirk@yahoo.com>
2022-04-22 17:57:27 -0700

    Revamp home page with flexbox and background image

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,19 +1,19 @@
-import profilepic from './images/GettyMichael.jpg'
+import profilepic from './images/GettyMichael_ccexpress.png'
 
 function Home() {
     return (
-        <div id="home" className="home">
-            <div className="title">
-                <h1>Hello, I'm Michael Kirk</h1>
-            </div>
-            <div className='info'>
+        <div className="home">
+            <div className='greeting'>
+                <h2>Hello, I'm</h2>
+                <h1>Michael Kirk</h1>
+                <br></br>
                 <p>
                     Bachelors of Science from the University of California Irvine.
                     <br></br>
                     Seeking full time employment for software engineer and related postions.
                 </p>
             </div>
-            <div className="profilepic">
+            <div className="profile-pic">
                 <img src={profilepic}></img>
             </div>
         </div>
