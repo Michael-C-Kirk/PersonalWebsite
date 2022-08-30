39025fb60d6f1e0d6d1d1640f4d2836ac0477f23
Michael Kirk <michael_c_kirk@yahoo.com>
2022-08-30 16:37:03 -0700

    Foodr project card created

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -1,12 +1,44 @@
 import { IconContext } from "react-icons";
 import { BsGithub } from "react-icons/bs";
+import icon1 from '../images/FoodrImage.png'
 
 function Projects() {
     return (
         <div className="projects-grid">
             <div className="card">
                 <div className="card-header card-image">
+                    <img src={icon1}></img>
+                </div>
+                <div className="card-title">
+                    <p>
+                        Foodr
+                    </p>
+                </div>
+                <div className="card-body">
+                    <i>
+                        HTML/CSS | JavaScript | Python
+                    </i>
+                    <p>
+                        Foodr was developed in a team of 3 which provided users with local restaurants without the need of a search. A decision tree classifier was used to ensure disliked options are shown less frequent in future restaurant retrievals. I was personally in charge of API information retrieval to be displayed to our front end. Google Places and Google Maps were both used to retrieve this information.
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <IconContext.Provider value={{ className: "links" }}>
+                        <a href="https://github.com/Michael-C-Kirk/Foodr">
+                            <BsGithub />
+                        </a>
+                    </IconContext.Provider>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="card-header card-image">
                     <img src="https://wallpaperaccess.com/full/1470705.jpg"></img>
+                </div>
+                <div className="card-title">
+                    <p>
+                        Project Name
+                    </p>
                 </div>
                 <div className="card-body">
                     <p>
@@ -14,7 +46,7 @@ function Projects() {
                     </p>
                 </div>
                 <div className="card-footer">
-                    <IconContext.Provider value={{ size: "2em", className: "links" }}>
+                    <IconContext.Provider value={{ className: "links" }}>
                         <a href="https://github.com/Michael-C-Kirk">
                             <BsGithub />
                         </a>
@@ -26,19 +58,10 @@ function Projects() {
                 <div className="card-header card-image">
                     <img src="https://wallpaperaccess.com/full/1470705.jpg"></img>
                 </div>
-                <div className="card-body">
+                <div className="card-title">
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Project Name
                     </p>
-                </div>
-                <div className="card-footer">
-
-                </div>
-            </div>
-
-            <div className="card">
-                <div className="card-header card-image">
-                    <img src="https://wallpaperaccess.com/full/1470705.jpg"></img>
                 </div>
                 <div className="card-body">
                     <p>
@@ -46,9 +69,14 @@ function Projects() {
                     </p>
                 </div>
                 <div className="card-footer">
-
+                    <IconContext.Provider value={{ className: "links" }}>
+                        <a href="https://github.com/Michael-C-Kirk">
+                            <BsGithub />
+                        </a>
+                    </IconContext.Provider>
                 </div>
             </div>
+
         </div>
     );
 }
