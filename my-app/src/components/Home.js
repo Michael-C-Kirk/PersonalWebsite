2900dae2760a760e7d0b4bd0e887d866fb13d26f
Michael Kirk <michael_c_kirk@yahoo.com>
2022-08-30 17:22:45 -0700

    Updated home page visuals

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -3,18 +3,22 @@ import profilepic from '../images/GettyMichael_ccexpress.png'
 function Home() {
     return (
         <div className="home">
-            <div className='greeting'>
-                <h2>Hello, I'm</h2>
-                <h1>Michael Kirk</h1>
-                <br></br>
-                <p>
-                    Bachelors of Science from the University of California Irvine.
+            <div className='greeting-card'>
+                <div className='greeting'>
+                    <h2>Hello, I'm</h2>
+                    <h1>Michael Kirk</h1>
                     <br></br>
-                    Seeking full time employment for software engineer and related postions.
-                </p>
+                    <p>
+                        Bachelors of Science from the University of California Irvine.
+                        <br></br>
+                        Seeking full time employment for software engineer and related postions.
+                    </p>
+                </div>
             </div>
-            <div className="profile-pic">
-                <img src={profilepic}></img>
+            <div className='profile-pic-container'>
+                <div className="profile-pic">
+                    <img src={profilepic}></img>
+                </div>
             </div>
         </div>
     );
