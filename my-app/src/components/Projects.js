8264e9686dbe382a4997662e27f9cf4bd5652244
Michael Kirk <michael_c_kirk@yahoo.com>
2022-09-01 15:48:22 -0700

    Added Alone Together Project Card

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -1,6 +1,7 @@
 import { IconContext } from "react-icons";
-import { BsGithub } from "react-icons/bs";
+import { BsGithub, BsLink45Deg } from "react-icons/bs";
 import icon1 from '../images/Foodr.gif'
+import icon2 from '../images/AloneTogether.jpg'
 
 function Projects() {
     return (
@@ -19,7 +20,7 @@ function Projects() {
                         HTML/CSS | JavaScript | Python
                     </i>
                     <p>
-                        Foodr was developed in a team of 3 which provided users with local restaurants without the need of a search. A decision tree classifier was used to ensure disliked options are shown less frequent in future restaurant retrievals. I was personally in charge of API information retrieval to be displayed to our front end. Google Places and Google Maps were both used to retrieve this information.
+                        Foodr was designed to provide users with local restaurants without the need of a search. A decision tree classifier was used to ensure disliked options are shown less frequent in future restaurant retrievals. I was personally in charge of API information retrieval to be displayed to our front end. Google Places and Google Maps were both used to retrieve this information.
                     </p>
                 </div>
                 <div className="card-footer">
@@ -33,22 +34,25 @@ function Projects() {
 
             <div className="card">
                 <div className="card-header card-image">
-                    <img src="https://wallpaperaccess.com/full/1470705.jpg"></img>
+                    <img src={icon2}></img>
                 </div>
                 <div className="card-title">
                     <p>
-                        Project Name
+                        Alone Together
                     </p>
                 </div>
                 <div className="card-body">
+                    <i>
+                        C# | Unity | Git
+                    </i>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Alone Together is a couch co-op rouge-like game. Each player takes the role of a cyborg bound together by a single life force and are tasked with stopping a robotic uprising on Earth. Some of my contributions within this project included player combat system integrations, mainly through player abilities, interactable objects, gameplay and level design.
                     </p>
                 </div>
                 <div className="card-footer">
-                    <IconContext.Provider value={{ className: "links" }}>
-                        <a href="https://github.com/Michael-C-Kirk">
-                            <BsGithub />
+                    <IconContext.Provider value={{ size: "1.25em", className: "links" }}>
+                        <a href="https://supriseorb.itch.io/alone-together">
+                            <BsLink45Deg />
                         </a>
                     </IconContext.Provider>
                 </div>
